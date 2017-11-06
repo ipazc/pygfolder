a494ed6066aff7ad3dce97049611dfdfb2521326
Iván <ipazce00@estudiantes.unileon.es>
2017-11-06 16:46:31 +0100

    Fixed typo in setup.py

--- setup.py
+++ setup.py
@@ -61,7 +61,7 @@ setup(name='pygfolder',
           'Programming Language :: Python :: 3.4',
           'Programming Language :: Python :: 3.5',
           'Programming Language :: Python :: 3.6',
-          'Programming Language :: Python :: 3.7',[
+          'Programming Language :: Python :: 3.7',
       ],
       test_suite='nose.collector',
       tests_require=['nose'],
